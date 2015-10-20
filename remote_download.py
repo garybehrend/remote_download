31829cb59a37a8580f3ccab1663cee43af7c56ed
philmccredden <phil.mccredden@gmail.com>
2015-10-20 15:09:38 +1100

    Object Orientated changed (__name__)

--- remote_download.py
+++ remote_download.py
@@ -13,6 +13,21 @@ import os
 from urllib.request import urlopen
 
 
+# Get url from user and check if valid
+def input_url(url):
+    print('Remote url downloader using dropbox @action')
+    url = input('Enter url to download (q=quit): ')
+    if url == 'q':
+        exit()
+    url_check = test_url(url)
+    if url_check:
+        print('added to download queue ...')
+    else:
+        print('Url is invalid!')
+    return
+
+
+# Test the url to see if valid
 def test_url(url):
     try:
         urlopen(url)
@@ -20,18 +35,16 @@ def test_url(url):
     except IOError:
         return False
 
-print('Remote url downloader using dropbox @action')
-url = input('Enter url to download (q=quit): ')
-if url == 'q':
-    exit()
-url_check = test_url(url)
-if url_check:
-    print('added to download queue ...')
-else:
-    print('Url is invalid!')
 
-output_path = os.path.expanduser('~/Dropbox/@Action/test.download')
-print(output_path)
-output_file = open(output_path, 'w')
-output_file.writelines(url)
-output_file.close()
+def main(url):
+    input_url(url)
+    output_path = os.path.expanduser('~/Dropbox/@Action/test.download')
+    print(output_path)
+    output_file = open(output_path, 'w')
+    output_file.writelines(url)
+    output_file.close()
+    return
+
+if __name__ == '__main__':
+    url = ""
+    main(url)
