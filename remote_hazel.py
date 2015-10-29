7d91255ea45f6196dbc38023d0de47b9ffae797d
philmccredden <phil.mccredden@gmail.com>
2015-10-29 11:34:21 +1100

    updated logging into function and used datetime instead of time

--- remote_hazel.py
+++ remote_hazel.py
@@ -16,10 +16,24 @@ import sys
 import urllib.request
 import os
 import logging
-import time
+import datetime
 
-# read name of file to download
-hazel_infile = sys.argv[1]
+
+def log(prog_name, log_action):
+    """log function - prog_name will be used to create the log file
+    in the format of prog_name.log as well as storing it in the log itself.
+    The log_action should be set to either start or finish and the Date
+    and time will be logged"""
+
+    file_name = prog_name + '.log'
+    logging.basicConfig(filename=file_name, level=logging.DEBUG)
+    today = datetime.date.today()
+    local_time = today.ctime()
+    logging.info(local_time + ' : ' + prog_name + ' : ' + log_action)
+
+prog_name = 'remote_download'  # set logging var
+
+hazel_infile = sys.argv[1]  # read name of file to download
 array = []
 with open(hazel_infile, "r") as f:
     for line in f:
@@ -30,22 +44,9 @@ url = array[0]
 saved_name = array[1]
 
 # Formulate path and file name to save
-saved_path = os.path.expanduser('~/hazel_downloads')
+saved_path = os.path.expanduser('~/hazel_downloads/')
 saved_fullpath = os.path.join(saved_path, saved_name)
 
-# Set up logging variables
-logging.basicConfig(filename='remote_download.log', level=logging.DEBUG)
-prog_name = 'remote_download'
-
-# Log start time
-localtime = time.asctime(time.localtime(time.time()))
-log_action = 'start'
-logging.info(localtime + ' : ' + prog_name + ' : ' + log_action)
-
-# Write to log file
-urllib.request.urlretrieve(url, saved_fullpath)
-
-# Log end time
-log_action = 'finish'
-localtime = time.asctime(time.localtime(time.time()))
-logging.info(localtime + ' : ' + prog_name + ' : ' + log_action)
+log(prog_name, 'start')  # Log start time
+urllib.request.urlretrieve(url, saved_fullpath)  # Write to file
+log(prog_name, 'finish')  # Log end time
